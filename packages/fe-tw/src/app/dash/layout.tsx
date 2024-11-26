bc92019711b57577d391ca9ac9e802a5b9f1e98e
maryjanyes <maryjanyes@gmail.com>
2024-11-26 16:32:40 +0200

    feat (49320): unify sidebar and navbar with daisyui

--- packages/fe-tw/src/app/dash/layout.tsx
+++ packages/fe-tw/src/app/dash/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../globals.css";
 import FooterAdmin from "@/components/Footers/FooterAdmin";
 import HeaderStats from "@/components/Headers/HeaderStats";
-import AdminNavbar from "@/components/Navbars/AdminNavbar";
-import Sidebar from "@/components/Sidebar/Sidebar";
-import { links } from "@/consts/nav";
+import FENavbar from "@/components/Navbar/FENavbar";
+
 import type React from "react";
 
 export const metadata: Metadata = {
@@ -20,16 +18,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<>
-			<Sidebar links={links} />
-			<div className="relative md:ml-64 bg-slate-100">
-				<AdminNavbar title="500 James Street" />
-				{/* Header */}
-				<HeaderStats />
-				<div className="px-4 md:px-10 mx-auto w-full -m-24">
-					{children}
-					<FooterAdmin />
+			<FENavbar linksForPage="admin" title="">
+				<div className="relative bg-slate-100">
+					<HeaderStats />
+					<div className="px-4 md:px-10 mx-auto w-full -m-24">
+						{children}
+					</div>
 				</div>
-			</div>
+			</FENavbar>
+			<FooterAdmin />
 		</>
 	);
 }
